c7a754664b2b1ed28a71c6f364411d046ba6c22b
duskrunner <stasvselin@gmail.com>
2023-09-13 01:17:43 +0300

    Shitty example to get started with js

--- calculator.js
+++ calculator.js
@@ -1,8 +1,47 @@
 const inputFieldEl = document.querySelector(".input-field");
 const numberButtons = document.querySelectorAll(".number");
+const operationButtons = document.querySelectorAll(".operation");
+const calcButton = document.querySelector(".calc");
+const backButton = document.querySelector(".backspace");
+const clearButton = document.querySelector(".clear");
+
 let a;
 let b;
 let operation;
-numberButtons.addEventListener('click', function()){
-    inputFieldEl = numberButtons.innerHTML;
-})
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const buttonValue = e.target.innerText;
+    if (inputFieldEl.innerHTML === "0") {
+      inputFieldEl.innerHTML = buttonValue;
+    } else {
+      inputFieldEl.innerHTML += buttonValue;
+    }
+  });
+});
+
+function saveOperation(e) {
+  operation = e.target.dataset.operation;
+  a = parseInt(inputFieldEl.innerHTML);
+  inputFieldEl.innerHTML = 0;
+}
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", saveOperation);
+});
+
+function calc() {
+  b = parseInt(inputFieldEl.innerHTML);
+  switch (operation) {
+    case "sum":
+      inputFieldEl.innerHTML = a + b;
+      break;
+
+    default:
+      break;
+  }
+}
+
+calcButton.addEventListener("click", () => {
+  calc();
+});
