6440f0ea9c23e1def1031ab5c23467dd453c4de3
Vertragana <136757182+Vertragana@users.noreply.github.com>
2023-09-14 00:19:28 +0300

    done

--- calculator.js
+++ calculator.js
@@ -35,8 +35,26 @@ function calc() {
   if (operation === 'sum') {
     inputFieldEl.innerHTML = a+b
   } else if (operation ==='sub') {
-
+    inputFieldEl.innerHTML = a-b
+  } else if(operation === 'multi'){
+    inputFieldEl.innerHTML = a*b
+  } else if(operation === 'div'){
+    inputFieldEl.innerHTML = a/b
   }
 }
 
 calcButton.addEventListener("click",calc);
+
+clearButton.addEventListener("click",(e) =>{
+  inputFieldEl.innerHTML = 0;
+})
+
+
+
+backButton.addEventListener("click", (e) =>{
+ if(inputFieldEl.innerHTML.length === 2) {
+  inputFieldEl.innerHTML = 0;
+ }
+else {inputFieldEl.innerHTML = inputFieldEl.innerHTML.slice(0, inputFieldEl.innerHTML.length - 2 );}
+  
+})
